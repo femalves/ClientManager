0adf964a4796dee4ab97709157e6e9a1094f7750
Fernanda Alves <fernanda__m@hotmail.com>
2020-01-17 14:37:49 -0300

    Updates

---
+++ precache-manifest.09fa880d64c7db6f0ca02e99eb06c2eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f930ff4554f8ce18557383d4e06a463",
+    "url": "/ClientManager/index.html"
+  },
+  {
+    "revision": "6316dc92549b3fb519d3",
+    "url": "/ClientManager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ed00e5bea4dbb01d8d4e",
+    "url": "/ClientManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "6316dc92549b3fb519d3",
+    "url": "/ClientManager/static/js/2.14577b85.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/ClientManager/static/js/2.14577b85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed00e5bea4dbb01d8d4e",
+    "url": "/ClientManager/static/js/main.e31071f9.chunk.js"
+  },
+  {
+    "revision": "9593576036fd473c7272",
+    "url": "/ClientManager/static/js/runtime-main.8d90af61.js"
+  }
+]);
